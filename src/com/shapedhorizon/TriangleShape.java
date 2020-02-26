8d6cb116921f034623f65895aa9fb0bde3cf5df1
Teymur <teymur.hasanzada@wetravel.com>
2020-02-26 15:16:32 +0400

    added center calc in constructor

--- src/com/shapedhorizon/TriangleShape.java
+++ src/com/shapedhorizon/TriangleShape.java
@@ -12,16 +12,19 @@ public class TriangleShape implements Shape {
             this.p1 = p1;
             this.p2 = p2;
             this.p3 = p3;
+            this.center = new PointBasic((p1.getX() + p2.getX() + p3.getX())/3, (p1.getY() + p2.getY() + p3.getY())/3, p1.getColor());
         }else{
             throw new RuntimeException("Invalid Triangle Params");
         }
     }
+
     public TriangleShape(VectorBasic l1, VectorBasic l2, VectorBasic l3){
         if(TriangleShape.isValid(l1, l2, l3)){
             this.p1 = l1.getP1();
             this.p2 = l1.getP2();
             if(l2.getP1().equals(this.p1) || l2.getP1().equals(this.p2)) this.p3 = l2.getP2();
             else this.p3 = l2.getP1();
+            this.center = new PointBasic((this.p1.getX() + this.p2.getX() + this.p3.getX())/3, (this.p1.getY() + this.p2.getY() + this.p3.getY())/3, this.p1.getColor());
         }else{
             throw new RuntimeException("Invalid Triangle Params");
         }
